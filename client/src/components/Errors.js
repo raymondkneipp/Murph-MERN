0b99ce01dcf0239925f3735f85700608c3cbb295
Raymond Kneipp III <raymond.f.kneipp@gmail.com>
2020-05-02 10:24:00 -0400

    Updated Error component styles

--- client/src/components/Errors.js
+++ client/src/components/Errors.js
@@ -1,16 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+
+const ErrorStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 24rem;
+  background-color: #2d3748;
+  padding: 0.8rem 1rem;
+  border-radius: 0.6rem;
+  margin: 0.5rem;
+  box-sizing: border-box;
+
+  & h4 {
+    font-size: 1rem;
+    color: #a0aec0;
+    margin: 0;
+    font-weight: normal;
+  }
+`;
 
 export const Errors = ({ errors }) => {
   return (
     <>
       {errors.map((error, index) => {
         return (
-          <div
-            key={index}
-            className="shadow bg-gray-800 max-w-xs flex items-center justify-center w-full max-w-xs rounded-lg px-3 py-2 my-2"
-          >
-            <h4 className="text-sm text-gray-500">{error.msg}</h4>
-          </div>
+          <ErrorStyle key={index}>
+            <h4>{error.msg}</h4>
+          </ErrorStyle>
         );
       })}
     </>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Container } from "../components/Container";
 import { AuthContext } from "../Store";
 
 export const Profile = ({ history }) => {
@@ -38,51 +39,53 @@ export const Profile = ({ history }) => {
 
   return (
     <div className="flex flex-col items-center justify-center flex-1 mx-2">
-      <div className="bg-gray-800 py-4 px-8 mb-6 shadow rounded-lg flex flex-col items-center box-border">
-        <h3 className="text-2xl font-medium text-gray-100 max-w-xs text-center mb-2">
-          {state.user && state.user.fname}{" "}
-          <span className="text-base text-gray-500">
-            {state.user && state.user.lname}
-          </span>
-        </h3>
-        <h4 className="text-gray-100 text-center mb-2">
-          Joined:{" "}
-          <span className="text-gray-500">
-            {state.user && new Date(state.user.date).toDateString()}
-          </span>
-        </h4>
+      <Container>
+        <div className="bg-gray-800 py-4 px-8 mb-6 shadow rounded-lg flex flex-col items-center box-border">
+          <h3 className="text-2xl font-medium text-gray-100 max-w-xs text-center mb-2">
+            {state.user && state.user.fname}{" "}
+            <span className="text-base text-gray-500">
+              {state.user && state.user.lname}
+            </span>
+          </h3>
+          <h4 className="text-gray-100 text-center mb-2">
+            Joined:{" "}
+            <span className="text-gray-500">
+              {state.user && new Date(state.user.date).toDateString()}
+            </span>
+          </h4>
 
-        <table className="table-auto">
-          <thead>
-            <tr>
-              <th>1st Mile Time</th>
-              <th>Calisthenics Time</th>
-              <th>2nd Time</th>
-              <th>Total Time</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {usersMurphs.map((murph) => {
-              return (
-                <tr key={murph._id}>
-                  <td>{murph.mileOneTime}</td>
-                  <td>{murph.calisthenicsTime}</td>
-                  <td>{murph.mileTwoTime}</td>
-                  <td>{murph.totalTime}</td>
-                  <td>
-                    {new Intl.DateTimeFormat("en-US", {
-                      year: "numeric",
-                      month: "long",
-                      day: "2-digit",
-                    }).format(new Date(murph.date))}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th>1st Mile Time</th>
+                <th>Calisthenics Time</th>
+                <th>2nd Time</th>
+                <th>Total Time</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {usersMurphs.map((murph) => {
+                return (
+                  <tr key={murph._id}>
+                    <td>{murph.mileOneTime}</td>
+                    <td>{murph.calisthenicsTime}</td>
+                    <td>{murph.mileTwoTime}</td>
+                    <td>{murph.totalTime}</td>
+                    <td>
+                      {new Intl.DateTimeFormat("en-US", {
+                        year: "numeric",
+                        month: "long",
+                        day: "2-digit",
+                      }).format(new Date(murph.date))}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </Container>
     </div>
   );
 };
