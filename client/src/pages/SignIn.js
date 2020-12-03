e02460de58a8d7b58527580809fc49e2ef47aa3e
thor3444 <raymond.f.kneipp@gmail.com>
2020-12-03 00:30:27 -0500

    Getting ready for deployment

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -6,126 +6,126 @@ import { Container } from "../components/Container";
 import AuthContext from "../context/auth/authContext";
 
 const SignInStyle = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
+	flex: 1;
+	display: flex;
+	align-items: center;
 
-  & > div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+	& > div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
-  & h1 {
-    color: #a0aec0;
-    font-weight: bold;
-    font-size: 2rem;
-    text-align: center;
-  }
+	& h1 {
+		color: #a0aec0;
+		font-weight: bold;
+		font-size: 2rem;
+		text-align: center;
+	}
 
-  & form {
-    display: flex;
-    flex-direction: column;
-    max-width: 24rem;
-    width: 100%;
+	& form {
+		display: flex;
+		flex-direction: column;
+		max-width: 24rem;
+		width: 100%;
 
-    & label {
-      color: #a0aec0;
-      margin-bottom: 0.5rem;
-    }
+		& label {
+			color: #a0aec0;
+			margin-bottom: 0.5rem;
+		}
 
-    & input {
-      border-radius: 0.6rem;
-      border: 0;
-      padding: 0.8rem 1rem;
-      font-size: 1rem;
-      margin-bottom: 0.5rem;
-      transition: all 0.2s;
+		& input {
+			border-radius: 0.6rem;
+			border: 0;
+			padding: 0.8rem 1rem;
+			font-size: 1rem;
+			margin-bottom: 0.5rem;
+			transition: all 0.2s;
 
-      &:focus {
-        outline: 0;
-        box-shadow: 0 0 0 0.2rem #63b3ed;
-      }
-    }
-  }
+			&:focus {
+				outline: 0;
+				box-shadow: 0 0 0 0.2rem #63b3ed;
+			}
+		}
+	}
 `;
 
 const SignInButton = styled.button`
-  margin: 1rem auto;
+	margin: 1rem auto;
 
-  color: #63b3ed;
-  transition: all 0.2s;
-  border: 0.1rem solid #63b3ed;
-  border-radius: 5rem;
-  padding: 1rem 1.2rem;
-  font-size: 1.1rem;
-  background-color: #1a202c;
-  cursor: pointer;
+	color: #63b3ed;
+	transition: all 0.2s;
+	border: 0.1rem solid #63b3ed;
+	border-radius: 5rem;
+	padding: 1rem 1.2rem;
+	font-size: 1.1rem;
+	background-color: #1a202c;
+	cursor: pointer;
 
-  &:focus {
-    outline: 0;
-    box-shadow: 0 0 0 0.2rem #63b3ed;
-  }
+	&:focus {
+		outline: 0;
+		box-shadow: 0 0 0 0.2rem #63b3ed;
+	}
 
-  &:hover {
-    color: #1a202c;
-    background-color: #63b3ed;
-  }
+	&:hover {
+		color: #1a202c;
+		background-color: #63b3ed;
+	}
 `;
 
 export const SignIn = ({ history }) => {
-  const [email, setEmail] = useState("ray@kay.com");
-  const [password, setPassword] = useState("123456");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  const { isAuthenticated, login, clearErrors } = useContext(AuthContext);
+	const { isAuthenticated, login, clearErrors } = useContext(AuthContext);
 
-  useEffect(() => {
-    if (isAuthenticated) {
-      history.push("/profile");
-    }
-  }, [isAuthenticated, history]);
+	useEffect(() => {
+		if (isAuthenticated) {
+			history.push("/profile");
+		}
+	}, [isAuthenticated, history]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    clearErrors();
+		clearErrors();
 
-    login({
-      email,
-      password,
-    });
-  };
+		login({
+			email,
+			password,
+		});
+	};
 
-  return (
-    <SignInStyle>
-      <Container>
-        <h1>
-          <FontAwesomeIcon icon={faSignInAlt} /> Sign In
-        </h1>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <label htmlFor="email">Email</label>
-          <input
-            required
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            required
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <SignInButton type="submit">
-            <FontAwesomeIcon icon={faSignInAlt} /> Sign In
-          </SignInButton>
-        </form>
-      </Container>
-    </SignInStyle>
-  );
+	return (
+		<SignInStyle>
+			<Container>
+				<h1>
+					<FontAwesomeIcon icon={faSignInAlt} /> Sign In
+				</h1>
+				<form onSubmit={(e) => handleSubmit(e)}>
+					<label htmlFor="email">Email</label>
+					<input
+						required
+						type="email"
+						id="email"
+						name="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<label htmlFor="password">Password</label>
+					<input
+						required
+						type="password"
+						id="password"
+						name="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<SignInButton type="submit">
+						<FontAwesomeIcon icon={faSignInAlt} /> Sign In
+					</SignInButton>
+				</form>
+			</Container>
+		</SignInStyle>
+	);
 };
